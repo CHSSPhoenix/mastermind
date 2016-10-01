cae9054f61952546ad0e5130fd4cc624cade3828
Carlos Gomez <carliros.g@gmail.com>
2016-10-01 14:21:22 -0400

    adding final work of the coding dojo

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,8 @@
 module Main where
-
+import Data.List  -- for randoms
 import System.Random  -- for randoms
 import System.IO      -- for hFlush
+import Data.Char      -- for isDigit
 
 type Row = [Int]
 type Guess = Row
@@ -20,6 +21,8 @@ main :: IO ()
 main =
   do
     s <- generateSolution -- initialization
+    --print s
+    --let s2 = ([1, 1, 1 , 1]::[Int])
     loop s                -- game loop
 
 -- The following function is given. It generates a random solution of the
@@ -37,24 +40,35 @@ loop :: Solution -> IO ()
 loop s =
   do
     i <- input            -- read (and parse) the user input
-    tODO (return ())
+    let (b,w,result) = check s i
+        res = report(b,w,result)
+    putStrLn res
+    if (result)
+     then return ()
+         else loop s
+    --tODO (return ())
 
 
 black, white :: Solution -> Guess -> Int
-black solution guess = tODO 0
-white solution guess = tODO 0
+black solution guess = length (filter (==True) (zipWith (==) solution guess))
+white solution guess = let cantidadBlancos = width-(length(solution \\ guess))
+                           cantidadNegros = black solution guess
+                       in cantidadBlancos - cantidadNegros
 
 check :: Solution -> Guess -> (Int,   -- number of black points,
                                Int,   -- number of white points
                                Bool)  -- all-correct guess?
-check solution guess = (black solution guess,
-                        white solution guess,
-                        tODO False)
+check solution guess
+  = let blacks = black solution guess
+        whites = white solution guess
+    in (blacks, whites, blacks == 4 && whites == 0)
 
 -- report is supposed to take the result of calling check, and
 -- produces a descriptive string to report to the player.
 report :: (Int, Int, Bool) -> String
-report (black, white, correct) = tODO ""
+report (black, white, correct)
+   = show black ++ " black, " ++ show white ++ " white"
+       ++ if correct then "\nCongratulations." else ""
 
 -- The function input is supposed to read a single guess from the
 -- player. The first three instructions read a line. You're supposed
@@ -65,7 +79,16 @@ input =
     putStr "? "
     hFlush stdout -- ensure that the prompt is printed
     l <- getLine
-    tODO (return [])
+    return $ transform l
+
+transform :: String -> Guess
+transform str = map toInt (words str)
+
+toInt :: String -> Int
+toInt (x:[]) = if isDigit x then read [x] else -1
+toInt _      = -1
+
+
 
 -- The following function |readInt| is given and parses a single
 -- integer from a string. It produces |-1| if the parse fails. You
